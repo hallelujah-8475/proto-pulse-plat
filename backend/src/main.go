0189e112c1c93fae411efc41b02f2ad7da7e4aad
hallelujah-8475 <doradoranobinobi93871293@gmail.com>
2024-09-30 12:49:30 +0900

    feat:環境構築

--- backend/src/main.go
+++ backend/src/main.go
@@ -6,19 +6,14 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// ブラウザに "Hello, World!" を表示
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello, World!!")
 }
 
 func main() {
-	// "/hello" というパスにハンドラーを登録
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/", helloHandler)
 
-	// サーバーを開始
-	fmt.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		// サーバーの起動に失敗した場合のエラーハンドリング
 		fmt.Println("Error starting server:", err)
 	}
 }
---
+++ build/frontend/Dockerfile
@@ -0,0 +1,3 @@
+FROM node:22.1.0
+WORKDIR /frontend
+CMD [ "sh","-c","npm run dev" ]
