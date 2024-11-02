275a0da0b898748c0ffc183aff423737424ee6d9
hallelujah-8475 <doradoranobinobi93871293@gmail.com>
2024-11-02 10:00:34 +0900

    refactor:リンク不具合修正

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -255,9 +255,7 @@ export default function Home() {
                       <h4 className="font-bold text-xl">{post.title}</h4>
                       <p className="mt-2 text-gray-600">{post.content}</p>
                       <div className="mt-5">
-                        <a
-                          href={`${process.env.BASE_URL}/post/detail?post_id=${post.id}`}
-                        >
+                        <a href={`/post/detail?post_id=${post.id}`}>
                           <button
                             type="button"
                             className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-gray-900"
