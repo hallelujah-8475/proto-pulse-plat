9eb2878db8933587517581787a4b2544ec2828d6
hallelujah-8475 <doradoranobinobi93871293@gmail.com>
2024-12-24 16:38:12 +0900

    refactor:configの修正

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  scripts: {
+    start: "PORT=${PORT:-3000} next start",
+  },
 };
 
 export default nextConfig;
