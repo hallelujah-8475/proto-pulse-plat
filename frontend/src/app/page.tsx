65e3ba4d845ace34fc6dd4833c980ef4709d84d8
hallelujah-8475 <doradoranobinobi93871293@gmail.com>
2025-04-12 16:03:40 +0900

    feat:色々実装・改修

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -110,6 +110,26 @@ export default function Home() {
 
   return (
     <>
+      <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
+        <div className="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
+          <div className="text-left">
+            <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-800 sm:text-5xl sm:leading-none md:text-4xl">
+              必要なものを
+              <span className="font-bold text-blue-500">必要な人へ</span>
+            </h2>
+            <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+              あなたのいらないが誰かの役に立つ
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pl-10">
+          <div className="relative w-full p-3 rounded  md:p-8">
+            <div className="rounded-lg bg-white text-black w-full">
+              <img src="https://picsum.photos/400/300" />
+            </div>
+          </div>
+        </div>
+      </div>
       <form className="max-w-lg mx-auto p-4" onSubmit={handleSearch}>
         <div className="flex relative">
           <button
@@ -226,7 +246,6 @@ export default function Home() {
       </div>
       <div className="flex justify-center mt-8 mb-5">
         <ol className="flex list-reset space-x-1">
-          {/* Previous Button */}
           <li>
             <button
               onClick={() => {
@@ -253,8 +272,6 @@ export default function Home() {
               </svg>
             </button>
           </li>
-
-          {/* Page Numbers */}
           {Array.from({ length: totalPages }, (_, index) => (
             <li key={index}>
               <button
@@ -269,8 +286,6 @@ export default function Home() {
               </button>
             </li>
           ))}
-
-          {/* Next Button */}
           <li>
             <button
               onClick={() => {
